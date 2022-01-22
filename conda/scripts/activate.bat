536d8577628f8a1a3a32012ac7401fbffe9dfb40
unknown <a.siniscalchi@bopen.eu>
2022-01-21 20:37:44 -0800

    bugfix: now setting HDF5_PLUGIN_PATH should work in windows

--- conda/scripts/activate.bat
+++ conda/scripts/activate.bat
@@ -1,6 +1,8 @@
 @echo off
-if [%HDF5_PLUGIN_PATH%] = [] (
+IF NOT DEFINED HDF5_PLUGIN_PATH (
     set HDF5_PLUGIN_PATH="%CONDA_PREFIX%\Library\bin"
 )
-if "%HDF5_PLUGIN_PATH%"=="%%HDF5_PLUGIN_PATH:%CONDA_PREFIX%\Library\bin=%"
-    set HDF5_PLUGIN_PATH="%HDF5_PLUGIN_PATH%;%CONDA_PREFIX%\Library\bin"
+
+set tempstring=%HDF5_PLUGIN_PATH%
+IF x%tempstring:%CONDA_PREFIX%\Library\bin=%==x%tempstring% (
+    set HDF5_PLUGIN_PATH="%HDF5_PLUGIN_PATH%;%CONDA_PREFIX%\Library\bin")
