f6bc6047f29c391abdfd23dc181e322a7328abc5
Marco Cucchi <m.cucchi@bopen.eu>
2022-02-08 20:03:26 +0100

    remove xarray dependency and change test accordingly

--- tests/test_10_nccopy_decompress.py
+++ tests/test_10_nccopy_decompress.py
@@ -16,7 +16,7 @@ import os
 import pytest
 import subprocess
 
-import xarray as xr
+import netCDF4 as nc
 
 import fcidecomp
 
@@ -33,25 +33,22 @@ BODY_UNCOMPR_FILEPATH = os.path.join(
     "W_XX-EUMETSAT-Darmstadt,IMG+SAT,MTI1+FCI-1C-RRAD-FDHSI-FD--CHK-BODY---"
     "NC4E_C_EUMT_20130804120845_GTT_DEV_20130804120330_20130804120345_N__T_0073_0015.nc"
 )
-BANDS = [
-    "ir_105", "ir_123", "ir_133", "ir_38", "ir_87", "ir_97",
-    "nir_13", "nir_16", "nir_22",
-    "vis_04", "vis_05", "vis_06", "vis_08", "vis_09",
-    "wv_63", "wv_73"
-]
-GROUPS = [f"data/{band}/measured" for band in BANDS]
+
 
 @pytest.mark.skipif(not os.environ["HDF5_PLUGIN_PATH"], reason="requires HDF5_PLUGIN_PATH in env")
 def test_decompression(tmpdir):
 
     uncompr_res_file = os.path.join(tmpdir, os.path.basename(BODY_UNCOMPR_FILEPATH))
-    process = subprocess.run(["nccopy", "-F", "none", BODY_COMPR_FILEPATH, uncompr_res_file])
+    process = subprocess.run(
+        f"nccopy -F none {BODY_COMPR_FILEPATH} {uncompr_res_file}", shell=True
+    )
 
-    for group in GROUPS:
-        ds_res = xr.open_dataset(uncompr_res_file, group=group)
-        ds_test = xr.open_dataset(BODY_UNCOMPR_FILEPATH, group=group)
-        assert ds_res.equals(ds_test)
-
+    for netcdf_file, txt_file in [
+        (uncompr_res_file, 'body_res.txt'),
+        (BODY_UNCOMPR_FILEPATH, 'body_test.txt')
+    ]:
+        command = f"ncdump -g measured -n decomp {netcdf_file} > {os.path.join(tmpdir, txt_file)}"
+        process = subprocess.run(command, shell=True)
 
 
 
