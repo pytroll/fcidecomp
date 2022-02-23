2206678ecbc638c93861f3e78ef3bf5db6cff65e
Marco Cucchi <m.cucchi@bopen.eu>
2022-02-23 11:38:54 +0100

    update __init__

--- src/fcidecomp-python/fcidecomp/__init__.py
+++ src/fcidecomp-python/fcidecomp/__init__.py
@@ -16,8 +16,8 @@ try:
 except KeyError:
     print("HDF5_PLUGIN_PATH environment variable is not defined")
 
-FCIDECOMP_ID = 32018
-FILTERS = {'fcidecomp': FCIDECOMP_ID}
+FILTER_NAME = 'fcidecomp'
+FILTER_ID = 32018
 
 try:
     _FilterRefClass = _h5py.filters.FilterRefBase
@@ -55,7 +55,7 @@ class FciDecomp(_FilterRefClass):
     """`
     `h5py.Group.create_dataset``'s compression arguments for using FciDecomp filter.
     """
-    filter_id = FCIDECOMP_ID
+    filter_id = FILTER_ID
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -68,46 +68,50 @@ def _init_filters():
     """
     hdf5_version = _h5py.h5.get_libversion()
 
-    for name, filter_id in FILTERS.items():
-        # Check if filter is already loaded (not on buggy HDF5 versions)
-        if (1, 8, 20) <= hdf5_version < (1, 10) or hdf5_version >= (1, 10, 2):
-            if _h5py.h5z.filter_avail(filter_id):
-                _logger.warning("%s filter already loaded, skip it.", name)
-                continue
+    filter_name = FILTER_NAME
+    filter_id = FILTER_ID
 
-        # Load DLL
-        if _sys.platform.startswith('win'):
-            filter_file_name = 'H5Zjpegls.dll'
-        elif _sys.platform.startswith('linux'):
-            filter_file_name = 'libH5Zjpegls.so'
-        filename = _glob(_os.path.join(PLUGINS_PATH, filter_file_name))
-        if len(filename):
-            filename = filename[0]
-        else:
-            _logger.error("Cannot initialize filter %s: File not found", name)
-            continue
-        try:
-            lib = _ctypes.CDLL(filename)
-        except OSError:
-            _logger.error("Failed to load filter %s: %s", name, filename)
-            continue
+    # Check if filter is already loaded (not on buggy HDF5 versions)
+    if (1, 8, 20) <= hdf5_version < (1, 10) or hdf5_version >= (1, 10, 2):
+        if _h5py.h5z.filter_avail(filter_id):
+            _logger.warning("%s filter already loaded, skip it.", filter_name)
+            return
 
-        if _sys.platform.startswith('win'):
-            # Use register_filter function to register filter
-            lib.register_filter.restype = _ctypes.c_int
-            retval = lib.register_filter()
-        else:
-            # Use init_filter function to initialize DLL and register filter
-            lib.init_filter.argtypes = [_ctypes.c_char_p]
-            lib.init_filter.restype = _ctypes.c_int
-            retval = lib.init_filter(
-                bytes(_h5py.h5z.__file__, encoding='utf-8'))
+    # Load DLL
+    if _sys.platform.startswith('win'):
+        filter_file_name = 'H5Zjpegls.dll'
+    elif _sys.platform.startswith('linux'):
+        filter_file_name = 'libH5Zjpegls.so'
+    else:
+        filter_file_name = ''
+    filename = _glob(_os.path.join(PLUGINS_PATH, filter_file_name))
+    if len(filename):
+        filename = filename[0]
+    else:
+        _logger.error("Cannot initialize filter %s: File not found", filter_name)
+        return
+    try:
+        lib = _ctypes.CDLL(filename)
+    except OSError:
+        _logger.error("Failed to load filter %s: %s", filter_name, filename)
+        return
 
-        if retval < 0:
-            _logger.error("Cannot initialize filter %s: %d", name, retval)
-            continue
+    if _sys.platform.startswith('win'):
+        # Use register_filter function to register filter
+        lib.register_filter.restype = _ctypes.c_int
+        retval = lib.register_filter()
+    else:
+        # Use init_filter function to initialize DLL and register filter
+        lib.init_filter.argtypes = [_ctypes.c_char_p]
+        lib.init_filter.restype = _ctypes.c_int
+        retval = lib.init_filter(
+            bytes(_h5py.h5z.__file__, encoding='utf-8'))
 
-        yield filename, lib
+    if retval < 0:
+        _logger.error("Cannot initialize filter %s: %d", filter_name, retval)
+        return
+
+    yield filename, lib
 
 
 _filters = dict(_init_filters())  # Store loaded filters
